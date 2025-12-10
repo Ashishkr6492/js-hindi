25701e11654a4aff8ecd4d8fa952e411d45ffa82
Ashish_krr <ashishkr6492@gmail.com>
2025-12-10 11:07:45 +0000

    conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,8 +21,38 @@ console.log(booleanIsLoggedIn);
 // "" => false
 // "ashish" => true
 
-let someNo = 33;
+// let someNo = 33;
 
-let stringNo = String(someNo)
-console.log(typeof stringNo);
-console.log(stringNo);
+// let stringNo = String(someNo)
+// console.log(typeof stringNo);
+// console.log(stringNo);
+
+// // *************** Operations *****************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2%3);
+// console.log(2**3);
+
+// let str1 = "hello"
+// let str2 = " ashish"
+// console.log(str1+str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+
+console.log(true);
+console.log(+true);
+
+let num1 , num2 , num3
+
+num1 = num2 =  num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
