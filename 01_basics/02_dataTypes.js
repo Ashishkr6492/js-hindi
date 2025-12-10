9569bb237248f1fba3d808b3a646f8b0ed49630a
Ashish_krr <ashishkr6492@gmail.com>
2025-12-10 09:15:22 +0000

    datatype

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,23 @@
 
 // alert(3+3) //we are using nodejs,not browser
 
+let name = "ashish"
+let age = 18
+let isLoggedIn = false
+
+// premitive datatype
+
+//number => 2 to power 53
+// bigint
+// string => " "
+//boolean => true/false
+//null => standalone value
+// undefined => now value is not assign
+//symbol => unique
+
+
+//object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
