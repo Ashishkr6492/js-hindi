4889d7d5f99f09247a43f2f8d5fdb28c8baa6e93
Ashish_krr <ashishkr6492@gmail.com>
2025-12-10 09:52:17 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "33abc"
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+// null => 0 , undefined => NaN
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true , 0=> false
+// "" => false
+// "ashish" => true
+
+let someNo = 33;
+
+let stringNo = String(someNo)
+console.log(typeof stringNo);
+console.log(stringNo);
