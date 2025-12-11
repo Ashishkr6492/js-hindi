c754ee04f8f48d061ab6791ac30853fe7baa23d5
Ashish_krr <ashishkr6492@gmail.com>
2025-12-11 15:07:13 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,35 @@
+// const name = "ashish"
+// const repoCount = 50
+
+// // console.log(name  + repoCount + " value");
+
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String("ash-uu")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('h'));
+
+// const newString = gameName.substring(0,4) // yaha pe -ve value nhi de skte h (last value not be include)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-5,3)
+// console.log(anotherString);
+
+const newStringOne = "   ashu   "
+console.log(newStringOne);
+console.log(newStringOne.trim()); // it remove space
+
+const url = "https://ashish.com/ashish%20kumar"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sudar'));
+console.log(gameName.split('-'));
+
