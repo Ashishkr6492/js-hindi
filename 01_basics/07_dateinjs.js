fd070d9a2681501c72d840f02c1b2a86d623c0d1
Ashish_krr <ashishkr6492@gmail.com>
2025-12-12 08:14:26 +0000

    date

--- 01_basics/07_dateinjs.js
+++ 01_basics/07_dateinjs.js
@@ -11,7 +11,21 @@ console.log(typeof myDate); //object
 // let myCreatedDate = new Date("2025-01-14") // yaha pe 1 se
 let myCreatedDate = new Date("01-14-2025")
 
-console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toLocaleString());
 
-let myTime
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000)); // ye abhi  ka samay denga
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday: "long"
+})
+
 
